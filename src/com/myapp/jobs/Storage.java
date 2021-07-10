2e5eeed66c5d6be338d979917d3d34cd1a9c6320
augustoaf <augustoaf@gmail.com>
2021-07-10 09:18:47 -0300

    wording fix

--- src/com/myapp/jobs/Storage.java
+++ src/com/myapp/jobs/Storage.java
@@ -24,7 +24,7 @@ public class Storage {
 	}
 	
 	public synchronized boolean add(Integer num) {
-		if (this.getCounter() < 1000) {//this if is because sometimes both threads are inside the while loop and the counter value is already updated to 1000 for one of the threads before the other thread calls the add method  
+		if (this.getCounter() < 1000) {//this is because sometimes both threads are inside the while loop and the counter value is already updated to 1000 for one of the threads before the other thread calls the add method  
 			this.setCounter(this.getCounter() + num);
 			return true;
 		}	
